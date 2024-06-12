f31404173f3ec45d00252df0a57a3c0abcf7e5b8
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-12 21:59:48 +0530

    register form completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
   return (
     <div>
       <Login/>
+      <Register/>
     </div>
   );
 }
