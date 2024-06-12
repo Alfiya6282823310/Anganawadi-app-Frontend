09c578c054227833fb3016771d00a689a62ead6b
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-12 22:52:31 +0530

    semidynamic completed search

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
-
+const [data,changeData]=useState(
+    {
+        "name":""
+    }
+)
+    const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+const readValue=()=>{
+    console.log(data)
+}
   return (
     <div>
         <div className="container">
@@ -10,9 +20,9 @@ const Search = () => {
                     <div className="row g-3">
                         <col className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
                         <label htmlFor="" className="form-label">children name</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='name' value={data.value} onChange={inputHandler}/>
                         <col className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
-                        <button className="btn btn-info">Search</button>
+                        <button className="btn btn-info" onClick={readValue}>Search</button>
                     </div>
                 </div>
             </div>
